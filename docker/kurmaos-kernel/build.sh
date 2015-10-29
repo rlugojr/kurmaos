75249f82885f9f74412b029e66be9f25761d9cd8
Ken Robertson <ken@apcera.com>
2015-10-29 08:35:30 -0700

    Update to 4.2.5 kernel. Simplify config file. Added stripping modules.
    Update to the 4.2.5 kernel.
    
    Move from checking in the full kernel config file to just the defconfig.
    This can be generated with "make savedefconfig" and reloaded with
    "make olddefconfig".
    
    Also added the parameter to have "--strip-unneeded" used on the modules.
    Try and save some space, and seems the safest option.

--- docker/kurmaos-kernel/build.sh
+++ docker/kurmaos-kernel/build.sh
@@ -9,14 +9,15 @@ emerge-webrsync
 emerge --sync
 
 # allow the proper kernel version
-echo '=sys-kernel/vanilla-sources-4.2.4 ~amd64' >> /etc/portage/package.accept_keywords
-emerge =sys-kernel/vanilla-sources-4.2.4
+echo '=sys-kernel/vanilla-sources-4.2.5 ~amd64' >> /etc/portage/package.accept_keywords
+emerge =sys-kernel/vanilla-sources-4.2.5
 mv /tmp/kernel.config /usr/src/linux/.config
 
 # compile it
 cd /usr/src/linux
+make olddefconfig
 make -j3
-make modules_install
+make INSTALL_MOD_STRIP="--strip-unneeded" modules_install
 cp arch/x86/boot/bzImage /boot/bzImage
 
 # cleanup
