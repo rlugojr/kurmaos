d9c1590e7fde782574b750c5e1bcf7a4ce6176a2
Ken Robertson <ken@apcera.com>
2015-11-16 17:10:02 -0800

    Added kurma-upgrader to the concourse pipeline

--- ci/rollout/prep.sh
+++ ci/rollout/prep.sh
@@ -11,3 +11,4 @@ mv kurma-cli-darwin-amd64/kurma-cli-darwin-amd64.tar.gz kurma-cli-$version-darwi
 mv kurma-server-linux-amd64/kurma-server-linux-amd64.tar.gz kurma-server-$version-linux-amd64.tar.gz
 mv disk-vmware-image/kurmaos-vmware.zip kurmaos-vmware-$version.zip
 mv disk-virtualbox-image/kurmaos-virtualbox.zip kurmaos-virtualbox-$version.zip
+mv kurma-upgrader-aci-image/kurma-upgrader.aci kurma-upgrader-$version.aci
