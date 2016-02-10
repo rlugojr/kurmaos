aac804fdb1e3589fac71ac899b906b137920745a
Ken Robertson <ken@apcera.com>
2016-02-10 15:41:09 -0800

    Add Concourse task for building AMIs

---
+++ packaging/disk-aws/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASE_PATH=`pwd`
+
+set -e -x
+
+# calculate ldflags for the version number
+version="$(git --git-dir=$BASE_PATH/kurma-source/.git describe --tags | cut -d'-' -f1)+git"
+if [[ -f $BASE_PATH/version/number ]]; then
+    version=$(cat $BASE_PATH/version/number)
+fi
+
+gunzip -k kurmaos-disk-image/kurmaos-disk.img.gz
+
+cd kurmaos-source/packaging/disk-aws
+./import.sh -B kurmaos-temp-disk-images \
+            -p $BASE_PATH/kurmaos-disk-image/kurmaos-disk.img \
+            -V $version \
+            -Z us-west-2a | tee $BASE_PATH/instances.txt
+
+# remove intermediate files to speed up concourse post-build ops
+rm $BASE_PATH/kurmaos-disk-image/kurmaos-disk.img
---
+++ packaging/disk-aws/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Change to the location of the script
+cd $(dirname $0)
+
+# Create the output file ahead of time to ensure it is available.
+output_filename=kurmaos-vmware.zip
+touch ../../output/$output_filename
+
+docker run --privileged --rm \
+       -v `pwd`/../../output/kurmaos-disk.img.gz:/tmp/build/kurmaos-disk-image/kurmaos-disk.img.gz \
+       -v `pwd`/../..:/tmp/build/kurmaos-source \
+       -v `pwd`/../../output/$output_filename:/tmp/build/$output_filename \
+       -w /tmp/build \
+       apcera/kurmaos-stage4 \
+       bash -c "./kurmaos-source/packaging/disk-vmware/build.sh && cp kurmaos.zip $output_filename"
+
+echo "Compiled output available in ../../output/$output_filename"
