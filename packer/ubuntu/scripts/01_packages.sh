d24790421075fa953572afa38b2c78265020119b
Ken Robertson <ken@apcera.com>
2016-03-07 11:09:45 -0800

    packer: Add in the universe package set

--- packer/ubuntu/scripts/01_packages.sh
+++ packer/ubuntu/scripts/01_packages.sh
@@ -1,6 +1,13 @@
 # Abort on error
 set -e -x
 
+# Get the codename and update the apt sources. This is to add in the universe
+# set of pacakges.
+ubuntuCodename=$(lsb_release -s -c)
+echo "deb http://us.archive.ubuntu.com/ubuntu/ $ubuntuCodename main restricted universe" > /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ $ubuntuCodename-security main restricted universe" >> /etc/apt/sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ $ubuntuCodename-updates main restricted universe" >> /etc/apt/sources.list
+
 # Update packages
 apt-get --yes --force-yes update
 
