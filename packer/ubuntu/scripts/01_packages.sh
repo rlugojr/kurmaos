f0dba01a77a8349b9e561337ae4b3ba03ed9461b
Ken Robertson <ken@apcera.com>
2016-03-10 15:21:28 -0800

    packer: On ubuntu 12.04, just download linux-image-extra and install it.

--- packer/ubuntu/scripts/01_packages.sh
+++ packer/ubuntu/scripts/01_packages.sh
@@ -10,11 +10,27 @@ echo "deb http://us.archive.ubuntu.com/ubuntu/ $ubuntuCodename-updates main rest
 
 # Update packages
 apt-get --yes --force-yes update
-apt-get --force-yes -fuy dist-upgrade
 
-# Install the specific needed linux-image-extra to get aufs
-extraPkg=$(dpkg -l | grep linux-image | grep -v linux-image-virtual | awk '{print $2}' | sed -e 's#linux-image#linux-image-extra#g')
-apt-get -y install $extraPkg
+# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+# Core problem: post-install scripts don't care that we told apt-get --yes/--force-yes
+DEBIAN_FRONTEND=noninteractive
+UCF_FORCE_CONFFNEW=yes
+export DEBIAN_FRONTEND UCF_FORCE_CONFFNEW
+ucf --purge /boot/grub/menu.lst
+apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+
+## NOTE: Ubuntu 12.04 does NOT like to install this package from the
+## percise-security/main repo when scripted with packer. It will when the same
+## script is ran locally on the via terminal, but never with Packer.
+if [[ "$ubuntuCodename" == "precise" ]]; then
+    wget -O /tmp/linux-image-extras.deb http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-image-extra-3.2.0-99-virtual_3.2.0-99.139_amd64.deb
+    dpkg -i /tmp/linux-image-extras.deb
+    rm /tmp/linux-image-extras.deb
+else
+    # Install the specific needed linux-image-extra to get aufs
+    extraPkg=$(dpkg -l | grep linux-image | grep -v linux-image-virtual | awk '{print $2}' | sed -e 's#linux-image#linux-image-extra#g')
+    apt-get -y install $extraPkg
+fi
 
 # Some needed apps/libraries
 apt-get -y install git libcap2 rsync
